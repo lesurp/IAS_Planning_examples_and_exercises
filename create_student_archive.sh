f30955a386f6e2716e8f1751ce139651d6f31e6b
Paul Lesur <git@lesurpaul.fr>
2023-01-03 15:10:12 +0100

    fix number

--- create_student_archive.sh
+++ create_student_archive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tar czf Exercise06.tar.gz --transform="flags=r;s|\(.*\).template|\1|" graph.py show_path.py grass_fire.py astar.py.template dijkstra.py.template
+tar czf Exercise05.tar.gz --transform="flags=r;s|\(.*\).template|\1|" graph.py show_path.py grass_fire.py astar.py.template dijkstra.py.template
