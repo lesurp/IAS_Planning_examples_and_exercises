a801e91215b02ecaee77853dcdd6552bef2fba52
Paul Lesur <git@lesurpaul.fr>
2023-01-05 17:15:18 +0100

    add files in students' archive

--- create_student_archive.sh
+++ create_student_archive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tar czf Exercise06.tar.gz --transform="flags=r;s|\(.*\).template|\1|" graph.py grass_fire.py common.py exercise_06.ipynb
+tar czf Exercise06.tar.gz --transform="flags=r;s|\(.*\).template|\1|" graph.py grass_fire.py common.py main.py jupy_main.py exercise_06.ipynb
